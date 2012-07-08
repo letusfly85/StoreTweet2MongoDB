1fc01d8480b43ddf4c4dd655ac3967bb9b9f24c5
shunsuke wada <letusfly95@gmail.com>
2012-07-08 15:18:01 +0900

    delete a parameter :return_flg

--- src/test.rb
+++ src/test.rb
@@ -6,12 +6,11 @@ include Insert2MongoDB
 
 #TODO consider how it should be about options(hash).
 #     It's not good like below.
-json_ary = twitter_stream(SAMPLE_STREAM,{:return_flg => 'hash' })
+json_ary = twitter_stream(SAMPLE_STREAM,{})
 json_ary.each {|json| insert2database($val_key[SAMPLE_STREAM].downcase,json)}
 
 json_ary = twitter_stream(FILTER_STREAM,
-			   { :post_parameters => ['job','haskell','change'],
-                             :return_flg      => 'hash' }
+			   { :post_parameters => ['job','haskell','change'] }
                          )
 
 json_ary.each {|json| insert2database($val_key[FILTER_STREAM].downcase,json)}
