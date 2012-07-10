20cf4804086878d700da44449a2732f966030fdc
shunsuke wada <letusfly95@gmail.com>
2012-07-10 10:31:13 +0900

    add TODO

--- src/utils.rb
+++ src/utils.rb
@@ -1,16 +1,10 @@
-#require 'net/http'
-#require 'kconv'
-#require './setup'
-
 module Utils
+#TODO consider how I code about Http Proxy.
 
     def recur_extend_uri(uri_name)
         return uri_name if (/\.html/) =~ uri_name
         if (/http\:\/\/bit\./   =~ uri_name) or
            (/http\:\/\/t\.co/   =~ uri_name)
-         #  (/http\:\/\/ow\.ly/  =~ uri_name) or
-         #  (/http\:\/\/htn\./   =~ uri_name) or
-         #  (/http\:\/\/dlvr/    =~ uri_name)
             expand_url(uri_name)
         else
             uri_name
@@ -21,9 +15,10 @@ module Utils
         uri = url.kind_of?(URI) ? url : URI.parse(url);
         Net::HTTP::Proxy(HTTP_PROXY_ADDR,HTTP_PROXY_PORT).start(uri.host, uri.port) do |io|
             r = io.head(uri.path);
-            URI.parse(r['Location']||uri.to_s).scheme ?
-                     ((r['Location']||uri.to_s) == url ?
-                      url : expand_url(r['Location']||uri.to_s)) : url;
+            URI.parse(r['Location']||uri.to_s).scheme ? (
+                        (r['Location']||uri.to_s) == url ?
+                        url : expand_url(r['Location']||uri.to_s)
+                      ) : url;
         end
     end
 
@@ -32,11 +27,3 @@ module Utils
         return title
     end
 end
-
-#include Utils
-#my_uri    = recur_extend_uri('http://t.co/XxwjqNQm')
-#puts my_uri
-
-#my_title  = get_html_title(my_uri)
-#puts my_title.length
-#puts my_title
